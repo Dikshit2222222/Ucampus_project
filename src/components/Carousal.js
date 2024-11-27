7791d9232f124ed0aa9b35a4883d67157f8d30d7
Dikshit2222222 <110338628+Dikshit2222222@users.noreply.github.com>
2024-11-27 20:41:26 +0530

    Add files via upload

---
+++ src/components/Carousal.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import burgerimg from "./burger.png";
+import momosimg from "./momos.jpg";
+import pizzaimg from "./pizza.jpg";
+import './Carousal.css'; 
+
+function Carousal() {
+  const [search, setSearch] = useState(""); 
+
+  return (
+    <div
+      id="carouselExampleCaptions"
+      className="carousel slide custom-carousel"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner">
+        {/* First Item */}
+        <div className="carousel-item active">
+          <img src={burgerimg} className="d-block w-100 custom-carousel-img" alt="Burger" />
+          <div className="carousel-caption">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+        </div>
+
+        {/* Second Item */}
+        <div className="carousel-item">
+          <img src={momosimg} className="d-block w-100 custom-carousel-img" alt="Momos" />
+          <div className="carousel-caption">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+        </div>
+
+        {/* Third Item */}
+        <div className="carousel-item">
+          <img src={pizzaimg} className="d-block w-100 custom-carousel-img" alt="Pizza" />
+          <div className="carousel-caption">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Carousel Controls */}
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
+
+export default Carousal;
